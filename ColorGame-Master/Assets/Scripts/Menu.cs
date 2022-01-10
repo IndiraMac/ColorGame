aa8732b6ef1804a3392c468e0fcc770341036869
Indira kar <indira@girzla.co.in>
2022-01-10 23:59:34 +0530

    Credit Updated

---
+++ ColorGame-Master/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    void Start()
+    {
+        ManagerScript.Instance.currentLevel = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Play(){
+        SceneManager.LoadScene("Level_01");
+    }
+
+}
---
+++ ColorGame-Master/README.md
@@ -0,0 +1,6 @@
+# Puzzle Game
+2020 / 3D Hyper-casual game
+
+https://berkekiran.itch.io/puzzle-game
+
+![Screenshot](https://user-images.githubusercontent.com/22173853/87536887-73525a80-c6a2-11ea-99e0-5634f3cb14cb.png)
